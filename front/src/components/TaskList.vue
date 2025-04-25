894ccad1f138bb069d322744a72f9bb2d3e5d748
estebanDC <esteban.da_costa@outlook.fr>
2025-04-25 11:42:43 +0200

    Simplify TaskList and improve styling.
    Removed commented-out code from TaskList for clarity and maintainability. Added a background color class to the body for consistent styling across the app.

--- front/src/components/TaskList.vue
+++ front/src/components/TaskList.vue
@@ -1,22 +1,9 @@
 <script setup lang="ts">
 import {taskStore} from "@/store/taskStore";
-
-// const tasks = taskStore.tasks
-
-
-// const res = await fetch("http://127.0.0.1:8080/tasks");
-// const data = await res.json();
-// console.log(tasks);
-// tasks.value = data;
-//
-// function removeFromList(id: number) {
-//   tasks.value = tasks.value!.filter(task => task.id !== id)
-// }
-
 </script>
 
 <template>
-  <UContainer class="p-8 min-h-screen bg-gray-50">
+  <UContainer class="p-8 min-h-screen">
     <Task v-for="task in taskStore.tasks" :key="task.id" :task="task" class="m-8" @deleteTask="taskStore.removeTask"/>
   </UContainer>
 </template>
